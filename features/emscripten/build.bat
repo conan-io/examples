1adfb5bd34eca814b3ba137130827108b32f6821
SSE4 <tomskside@gmail.com>
2019-04-30 12:23:17 +0700

    - add Emscripten to CI
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- features/emscripten/build.bat
+++ features/emscripten/build.bat
@@ -1,4 +1,5 @@
 @echo off
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 conan remove conan-hello-emscripten/* -f
 conan create . conan/testing  -k -p emscripten.profile --build missing
 conan install conanfile.txt  -pr emscripten.profile
