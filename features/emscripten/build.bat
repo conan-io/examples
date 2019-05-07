a07ef12ca0f67ae6b4e7322a963c0d28e072a741
Daniel <danimanzaneque@gmail.com>
2019-05-07 14:46:48 +0700

    Update features/emscripten/build.bat
    Co-Authored-By: SSE4 <tomskside@gmail.com>

--- features/emscripten/build.bat
+++ features/emscripten/build.bat
@@ -1,5 +1,4 @@
 @echo off
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 conan remove conan-hello-emscripten/* -f
 conan create . conan/testing  -k -p emscripten.profile --build missing
 conan install conanfile.txt  -pr emscripten.profile
