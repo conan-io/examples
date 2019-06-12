7cc8fbe6b33b999c28acf3e011c73c64972717ca
danimtb <danimanzaneque@gmail.com>
2019-06-12 13:38:03 +0200

    Adds Multi config package (N configs -> 1 package) example

---
+++ features/multi_config/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "hello.h"
+
+void hello(){
+    #if!defined NDEBUG
+        std::cout << "Hello World Debug!" <<std::endl;
+    #else
+        std::cout << "Hello World Release!" <<std::endl;
+    #endif
+}
