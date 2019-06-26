b6aedca0e99139aecac9d7eecc1cc72e8b1df4ad
czoido <mrgalleta@gmail.com>
2019-06-26 09:08:36 +0200

    fix windows bat

--- libraries/dear-imgui/basic/build.bat
+++ libraries/dear-imgui/basic/build.bat
@@ -5,7 +5,7 @@ MKDIR build
 PUSHD build
 
 conan install ..
-cmake .. -G "Visual Studio 15 2017 Win64"
+cmake .. -G "%CMAKE_GENERATOR%"
 cmake --build . --config Release
 
 Release\dear-imgui-conan.exe
