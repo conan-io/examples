160a65e17dda3dcc8cad0e3b2b1edfbf01ed4705
Uilian Ries <uilianries@gmail.com>
2019-07-11 12:17:59 -0300

    #24 emscript short path
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- features/emscripten/build.bat
+++ features/emscripten/build.bat
@@ -1,4 +1,6 @@
 @echo off
+set CONAN_USER_HOME_SHORT=1
+set CONAN_USE_ALWAYS_SHORT_PATHS=1
 conan remove conan-hello-emscripten/* -f
 conan create . conan/testing  -k -p emscripten.profile --build missing
 conan install conanfile.txt  -pr emscripten.profile
