53c28da4197dafd69f281b5568d97633c8232464
Uilian Ries <uilianries@gmail.com>
2019-07-12 14:24:01 -0300

    #24 Remove only custom home path
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- features/emscripten/build.bat
+++ features/emscripten/build.bat
@@ -1,7 +1,6 @@
 @echo off
-set CONAN_USER_HOME=
-set CONAN_USER_HOME_SHORT=1
-set CONAN_USE_ALWAYS_SHORT_PATHS=1
+set CONAN_USER_HOME=%USERPROFILE%
+rd /s /q %USERPROFILE%/.conan
 conan remove conan-hello-emscripten/* -f
 conan create . conan/testing  -k -p emscripten.profile --build missing
 conan install conanfile.txt  -pr emscripten.profile
