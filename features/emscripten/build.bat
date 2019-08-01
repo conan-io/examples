aa3073cd26e0872d4f0baba4c0e2cad652d2d6f0
Uilian Ries <uilianries@gmail.com>
2019-08-01 08:26:38 -0300

    Revert "#24 Fix path for emscripten"
    This reverts commit 6c713d72e5dcb84426d445eac8a427c2a44d9c91.

--- features/emscripten/build.bat
+++ features/emscripten/build.bat
@@ -1,7 +1,5 @@
 @echo off
 
-set CONAN_USER_HOME=C:/projects/CONAN_HOME/emscripten
-
 conan remove conan-hello-emscripten/* -f
 conan create . conan/testing  -k -p emscripten.profile --build missing
 conan install conanfile.txt  -pr emscripten.profile
