d193029e21720b6dd7d415a1a928206feb666aaa
Carlos Zoido <mrgalleta@gmail.com>
2019-08-08 10:34:43 +0200

    Handle c++11 settings in waf build helper Issue/31  (#33)
    * handle cppstd setting
    
    * build example with c++11 settings
    
    * minor changes

--- features/integrate_build_system/build.sh
+++ features/integrate_build_system/build.sh
@@ -3,7 +3,7 @@
 conan create waf-generator user/channel
 conan create waf-installer user/channel
 conan create waf-build-helper user/channel
-conan create waf-mylib user/channel
+conan create waf-mylib user/channel -s compiler.cppstd=11
 
 set -e
 set -x
@@ -13,5 +13,5 @@ rm -rf build
 mkdir build
 
 conan source . --source-folder=build
-conan install . --install-folder=build
+conan install . --install-folder=build -s compiler.cppstd=11
 conan build . --build-folder=build
