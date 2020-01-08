07b79eebe6ea4a11370c21106c220a7e4465de22
czoido <mrgalleta@gmail.com>
2020-01-08 17:44:09 +0100

    revert emscripten example changes

--- features/emscripten/build.bat
+++ features/emscripten/build.bat
@@ -1,5 +1,5 @@
 @echo off
 
 conan remove conan-hello-emscripten/* -f
-conan create . conan/testing  -k -p emscripten.profile
+conan create . conan/testing  -k -p emscripten.profile --build missing
 conan install conanfile.txt  -pr emscripten.profile
--- features/emscripten/build.sh
+++ features/emscripten/build.sh
@@ -3,5 +3,5 @@
 set -ex
 
 conan remove conan-hello-emscripten/* -f
-conan create . conan/testing  -k -p emscripten.profile
+conan create . conan/testing  -k -p emscripten.profile --build missing
 conan install conanfile.txt  -pr emscripten.profile
