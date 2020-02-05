279aec528ac02b20e28b6df4146f6927028106bf
Uilian Ries <uilianries@gmail.com>
2020-02-05 10:39:01 -0300

    #43 Install imgui from CCI
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- libraries/dear-imgui/basic/build.bat
+++ libraries/dear-imgui/basic/build.bat
@@ -4,6 +4,8 @@ RMDIR /Q /S build
 MKDIR build
 PUSHD build
 
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan & true
+
 conan install ..
 cmake .. -G "%CMAKE_GENERATOR%"
 cmake --build . --config Release
