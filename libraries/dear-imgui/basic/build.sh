a5f4e2e968a448fd6dc6ba8a1b53e2abd4fa7a6c
Uilian Ries <uilianries@gmail.com>
2020-02-06 15:29:25 -0300

    #43 Fix Linux build order
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- libraries/dear-imgui/basic/build.sh
+++ libraries/dear-imgui/basic/build.sh
@@ -7,7 +7,7 @@ rm -rf build
 mkdir build
 pushd build
 
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan | true
 
 conan install ..
 cmake .. -DCMAKE_BUILD_TYPE=Release
