ca2c5d05283727bc52ef983377053154e327845e
Uilian Ries <uilianries@gmail.com>
2020-02-06 14:46:20 -0300

    Do not change the cache folder
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- libraries/dear-imgui/basic/build.bat
+++ libraries/dear-imgui/basic/build.bat
@@ -4,8 +4,10 @@ RMDIR /Q /S build
 MKDIR build
 PUSHD build
 
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan & true
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 
 conan install ..
 cmake .. -G "%CMAKE_GENERATOR%"
 cmake --build . --config Release
+
+conan remote remove bincrafters
--- libraries/dear-imgui/basic/build.sh
+++ libraries/dear-imgui/basic/build.sh
@@ -7,8 +7,10 @@ rm -rf build
 mkdir build
 pushd build
 
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan | true
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 
 conan install ..
 cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build .
+
+conan remote remove bincrafters
