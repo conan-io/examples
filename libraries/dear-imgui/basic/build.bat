2eae97380fa53e3b5c14013d8530275dd42e45af
Uilian Ries <uilianries@gmail.com>
2020-02-07 05:48:17 -0300

    #43 Fix broken imgui build (#45)
    * #43 Fix broken imgui build
    
    Both GLFW and GLEW requires packages (mesa and X11) which
    are not listed in Conan Center. The possible functional
    example for now, is creating and destroying context,
    with no graphical support.
    
    We can update it in the future, but first we need to
    solve the graphical libs in Conan Center Index
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Revert "#43 Fix broken imgui build"
    
    This reverts commit 2ede1db33103c6fadbf4e5f4ef89b74bcbd7a14e.
    
    * #43 Install imgui from CCI
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Change imgui targets order
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Do not change the cache folder
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * -a
    
    Revert cmakefile for imgui
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Update conanfile for imgui
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #43 Fix Linux build order
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- libraries/dear-imgui/basic/build.bat
+++ libraries/dear-imgui/basic/build.bat
@@ -4,6 +4,10 @@ RMDIR /Q /S build
 MKDIR build
 PUSHD build
 
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+
 conan install ..
 cmake .. -G "%CMAKE_GENERATOR%"
 cmake --build . --config Release
+
+conan remote remove bincrafters
--- libraries/dear-imgui/basic/build.sh
+++ libraries/dear-imgui/basic/build.sh
@@ -7,6 +7,10 @@ rm -rf build
 mkdir build
 pushd build
 
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan | true
+
 conan install ..
 cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build .
+
+conan remote remove bincrafters
