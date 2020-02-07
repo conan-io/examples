cd38be65dc2396c716211b3304f2899214a83419
jgsogo <jgsogo@gmail.com>
2020-02-07 15:52:26 +0100

    typo

--- libraries/protobuf/serialization/build.sh
+++ libraries/protobuf/serialization/build.sh
@@ -15,5 +15,5 @@ bin/sensor
 
 
 python -m venv _exproto
-source _exproto/bin/activate && pip install -U protobuf && 
+source _exproto/bin/activate && pip install -U protobuf
 _exproto/bin/python ../main.py
