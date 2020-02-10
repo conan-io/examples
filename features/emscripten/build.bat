2959ef36f457908cb9c8a9cc2f5fdda8f05ab2e0
jgsogo <jgsogo@gmail.com>
2020-02-10 19:10:32 +0100

    emsdk example requires x64

--- features/emscripten/build.bat
+++ features/emscripten/build.bat
@@ -2,4 +2,4 @@
 
 conan remove conan-hello-emscripten/* -f
 conan create . conan/testing  -pr emscripten.profile -s arch_build=x86_64 --build missing
-conan install conanfile.txt  -pr emscripten.profile
+conan install conanfile.txt  -pr emscripten.profile -s arch_build=x86_64
