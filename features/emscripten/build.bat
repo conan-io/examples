bd521d1c7b4a194068e65c75fd4c34c26d927c10
jgsogo <jgsogo@gmail.com>
2020-02-10 17:14:08 +0100

    pass setting x86_64

--- features/emscripten/build.bat
+++ features/emscripten/build.bat
@@ -1,5 +1,5 @@
 @echo off
 
 conan remove conan-hello-emscripten/* -f
-conan create . conan/testing  -k -p emscripten.profile --build missing
+conan create . conan/testing  -pr emscripten.profile -s arch_build=x86_64 --build missing
 conan install conanfile.txt  -pr emscripten.profile
