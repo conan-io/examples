11928527ff4ca436daced768779e9be9e95ee569
czoido <mrgalleta@gmail.com>
2020-03-04 12:41:35 +0100

    use python script

--- features/integrate_build_system/build.bat
+++ features/integrate_build_system/build.bat
@@ -1,16 +1,1 @@
-@ECHO ON
-
-conan create waf-generator user/channel
-conan create waf-installer user/channel
-conan create waf-build-helper user/channel
-conan create waf-mylib user/channel
-
-PUSHD mylib-consumer
-RMDIR /Q /S build
-MKDIR build
-
-conan source . --source-folder=build
-conan install . --install-folder=build
-conan build . --build-folder=build
-
-POPD
+python build.py
