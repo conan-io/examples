9c975d477a2f2d69c23bafa9eefcbbd870609dc1
Uilian Ries <uilianries@gmail.com>
2020-04-27 16:03:08 -0300

    Force mac os version 2
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- libraries/gtest/encryption/build.sh
+++ libraries/gtest/encryption/build.sh
@@ -15,10 +15,10 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     conan test test_package conan-gtest-example/0.1.0@conan/testing -s build_type=Debug --build missing -s os.version=10.13
 else
     conan source . --source-folder source
-    conan install . --install-folder build -s build_type=Debug
+    conan install . --install-folder build -s build_type=Debug --build missing
     conan build . --source-folder source --build-folder build
     conan package . --source-folder source --build-folder build --package-folder package
     conan export-pkg . conan/testing --package-folder package
 
-    conan test test_package conan-gtest-example/0.1.0@conan/testing -s build_type=Debug
+    conan test test_package conan-gtest-example/0.1.0@conan/testing -s build_type=Debug --build missing
 fi
