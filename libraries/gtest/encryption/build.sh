f0624c26f6debe59b69ddfd81d7cf700e647d874
Uilian Ries <uilianries@gmail.com>
2020-04-27 15:36:13 -0300

    Force mac os version
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- libraries/gtest/encryption/build.sh
+++ libraries/gtest/encryption/build.sh
@@ -5,10 +5,20 @@ set -x
 
 rm -rf build source package
 
-conan source . --source-folder source
-conan install . --install-folder build -s build_type=Debug
-conan build . --source-folder source --build-folder build
-conan package . --source-folder source --build-folder build --package-folder package
-conan export-pkg . conan/testing --package-folder package
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    conan source . --source-folder source
+    conan install . --install-folder build -s build_type=Debug --build missing -s os.version=10.13
+    conan build . --source-folder source --build-folder build
+    conan package . --source-folder source --build-folder build --package-folder package
+    conan export-pkg . conan/testing --package-folder package
 
-conan test test_package conan-gtest-example/0.1.0@conan/testing -s build_type=Debug
+    conan test test_package conan-gtest-example/0.1.0@conan/testing -s build_type=Debug --build missing -s os.version=10.13
+else
+    conan source . --source-folder source
+    conan install . --install-folder build -s build_type=Debug
+    conan build . --source-folder source --build-folder build
+    conan package . --source-folder source --build-folder build --package-folder package
+    conan export-pkg . conan/testing --package-folder package
+
+    conan test test_package conan-gtest-example/0.1.0@conan/testing -s build_type=Debug
+fi
