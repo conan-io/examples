585ad883cb9a57c9f7f23ccec5b764595e09fa0a
Uilian Ries <uilianries@gmail.com>
2020-04-27 15:26:38 -0300

    Force debug mode for gtest
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- libraries/gtest/encryption/build.sh
+++ libraries/gtest/encryption/build.sh
@@ -6,9 +6,9 @@ set -x
 rm -rf build source package
 
 conan source . --source-folder source
-conan install . --install-folder build
+conan install . --install-folder build -s build_type=Debug
 conan build . --source-folder source --build-folder build
 conan package . --source-folder source --build-folder build --package-folder package
 conan export-pkg . conan/testing --package-folder package
 
-conan test test_package conan-gtest-example/0.1.0@conan/testing
+conan test test_package conan-gtest-example/0.1.0@conan/testing -s build_type=Debug
