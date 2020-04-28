d5a6bec70f83e968205b12f1005495e6162b5bc6
SSE4 <tomskside@gmail.com>
2020-04-28 19:48:18 +0700

    - forget to commit file
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- libraries/gtest/encryption/encrypter.h
+++ libraries/gtest/encryption/encrypter.h
@@ -6,5 +6,5 @@
 
 int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *iv, unsigned char *ciphertext);
 int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, unsigned char *iv, unsigned char *plaintext);
-void handleErrors(void);
+void handleErrors(const char *);
 
