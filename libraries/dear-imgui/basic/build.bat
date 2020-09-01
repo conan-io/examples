a1fec7cd74dcea8bea2ccd416c8f3c4e4f26f267
Carlos Zoido <mrgalleta@gmail.com>
2020-09-01 14:55:42 +0200

    Get cmake generator from environment in windows for lockfile example (#70)
    * get generator from environment
    
    * print message and exit if not CMAKE_GENERATOR
    
    * fix format
    
    * set versions for examples

--- libraries/dear-imgui/basic/build.bat
+++ libraries/dear-imgui/basic/build.bat
@@ -1,9 +1,14 @@
-@ECHO ON
+if "%CMAKE_GENERATOR%"=="" (
+    ECHO CMAKE_GENERATOR environment variable not defined. Please define the CMake generator in the CMAKE_GENERATOR environment variable.
+)
+else (
+    @ECHO ON
 
-RMDIR /Q /S build
-MKDIR build
-PUSHD build
+    RMDIR /Q /S build
+    MKDIR build
+    PUSHD build
 
-conan install ..
-cmake .. -G "%CMAKE_GENERATOR%"
-cmake --build . --config Release
+    conan install ..
+    cmake .. -G "%CMAKE_GENERATOR%"
+    cmake --build . --config Release
+)
--- libraries/protobuf/serialization/build.bat
+++ libraries/protobuf/serialization/build.bat
@@ -1,15 +1,20 @@
-@ECHO ON
+if "%CMAKE_GENERATOR%"=="" (
+    ECHO CMAKE_GENERATOR environment variable not defined. Please define the CMake generator in the CMAKE_GENERATOR environment variable.
+)
+else (
+    @ECHO ON
 
-RMDIR /Q /S build
-MKDIR build
-PUSHD build
+    RMDIR /Q /S build
+    MKDIR build
+    PUSHD build
 
-conan install ..
-cmake .. -G "%CMAKE_GENERATOR%"
-cmake --build . --config Release
+    conan install ..
+    cmake .. -G "%CMAKE_GENERATOR%"
+    cmake --build . --config Release
 
-bin\sensor.exe
+    bin\sensor.exe
 
-pip install protobuf
-python ../main.py
-pip uninstall -y protobuf
+    pip install protobuf
+    python ../main.py
+    pip uninstall -y protobuf
+)
