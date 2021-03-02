813e8cef7607e1ff9a979f707bef7894daa07abc
Carlos Zoido <mrgalleta@gmail.com>
2021-03-02 08:57:35 +0100

    Update run.py

--- .ci/run.py
+++ .ci/run.py
@@ -111,10 +111,10 @@ def get_conan_env(script):
 
 
 def configure_profile(env):
-    subprocess.Popen(["conan", "profile", "new", "default", "--detect"], stderr=subprocess.STDOUT, env=env).communicate()[0]
+    subprocess.Popen(["conan", "profile", "new", "default", "--detect"], stderr=subprocess.STDOUT, env=env).communicate()
     if platform.system() == "Linux":
         subprocess.Popen(["conan", "profile", "update", "settings.compiler.libcxx=libstdc++11", "default"],
-                          stderr=subprocess.STDOUT, env=env).communicate()[0]
+                          stderr=subprocess.STDOUT, env=env).communicate()
 
 
 def print_build(script):
