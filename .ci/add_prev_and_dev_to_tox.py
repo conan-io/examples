e66137a3dec2e4daa932954fbc2f91f93b32fb75
czoido <mrgalleta@gmail.com>
2021-10-06 20:46:56 +0200

    restrict prev and dev to master

--- .ci/add_prev_and_dev_to_tox.py
+++ .ci/add_prev_and_dev_to_tox.py
@@ -29,9 +29,11 @@ def update_tox(last_version):
     with open(tox_file, 'r') as f:
         content = f.read()
 
-    assert 'conanprev: conan-unknown' in content, "Unexpected tox.ini content"
+    more_versions = """conancurrent: conan
+    conandev: git+https://github.com/conan-io/conan.git@develop
+    conanprev: {}""".format(conan_prev)
 
-    content = content.replace('conanprev: conan-unknown', 'conanprev: {}'.format(conan_prev))
+    content = content.replace('conancurrent: conan', more_versions)
 
     with open(tox_file, 'w') as f:
         f.write(content)
