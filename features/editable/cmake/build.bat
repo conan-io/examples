6eaf6646bd1d321991428dbe016d8ba69c68fc78
James <james@conan.io>
2021-10-07 18:15:24 +0200

    Feature/new editable (#94)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * linux
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    Co-authored-by: czoido <mrgalleta@gmail.com>

--- features/editable/cmake/build.bat
+++ features/editable/cmake/build.bat
@@ -1,27 +1,51 @@
 @ECHO ON
 
-RMDIR /Q /S say/build
-RMDIR /Q /S hello/build
+RMDIR /Q /S say\build
+RMDIR /Q /S hello\build
 
-MKDIR "say/build/Release"
-MKDIR "hello/build"
+REM Put say package in editable mode, and build it
+conan editable add say/ say/0.1@user/channel
 
-conan export say/ say/0.1@user/channel
-conan editable add say/ say/0.1@user/channel --layout=layout_vs
-
-PUSHD "say/build/Release"
-
-conan install ../..
-cmake ../../src
+PUSHD "say"
+REM It is very important to install 2 configurations, if you are building later the 2 configs, so the toolchain is multi-config (runtime libs)
+REM There is a bug in 1.40, this needs to be done in the root folder
+conan install . -s build_type=Release
+conan install . -s build_type=Debug
+MKDIR "build"
+PUSHD "build"
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan/conan_toolchain.cmake
 cmake --build . --config Release
-
+cmake --build . --config Debug
 POPD
+POPD
+
+REM Build hello consumer
+MKDIR "hello/build"
 PUSHD "hello/build"
-
-conan install .. 
-cmake ../src/
+conan install .. -s build_type=Release
+conan install .. -s build_type=Debug
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
 cmake --build . --config Release
+cmake --build . --config Debug
+"Release/hello.exe"
+"Debug/hello.exe"
+POPD
 
-"bin/hello.exe"
+REM Do a modification in the source code of the editable say
+PUSHD "say/build"
+COPY ..\src\say2.cpp ..\src\say.cpp /Y
+TOUCH ..\src\say.cpp
+cmake --build . --config Release
+cmake --build . --config Debug
+POPD
+
+REM Build and execute the consumer depending on the editable
+PUSHD "hello/build"
+cmake --build . --config Release
+cmake --build . --config Debug
+"Release/hello.exe"
+"Debug/hello.exe"
+POPD
+
 
 conan editable remove say/0.1@user/channel
--- features/editable/cmake/say/conanfile.py
+++ features/editable/cmake/say/conanfile.py
@@ -1,21 +1,46 @@
-from conans import ConanFile, CMake
+from conans import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake
+from conan.tools.layout import cmake_layout
 
-class Pkg(ConanFile):
+
+class SayConan(ConanFile):
     name = "say"
     version = "0.1"
-    settings = "os", "compiler", "arch", "build_type"
-    generators = "cmake"
-    exports_sources = "src/*"
+
+    # Optional metadata
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Say here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "src/*"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="src")
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy("*.h", src="src", dst="include")
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        cmake = CMake(self)
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = ["say"]
