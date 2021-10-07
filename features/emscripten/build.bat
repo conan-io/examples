b8f40edaf2a6375f92b8bbad98c5fd59a3095a12
czoido <mrgalleta@gmail.com>
2021-10-07 15:03:15 +0200

    wip

--- features/emscripten/build.bat
+++ features/emscripten/build.bat
@@ -2,4 +2,4 @@
 
 conan remove conan-hello-emscripten/* -f
 conan create . conan/testing  --profile:host=emscripten.profile --profile:build=default --build missing
-conan install conanfile.txt  --profile:host=emscripten.profile --profile:build=default
+conan install conanfile.txt  --profile:host=emscripten.profile --profile:build=default --build missing
