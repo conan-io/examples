e2705d2859546d83a217e07ec86ddc8ce6845450
César García Tapia <cesar.garcia.tapia@gmail.com>
2021-11-30 10:51:08 +0100

    Trying visual studio examples with msvc 19.16

--- features/visual_studio/creating_packages/build.bat
+++ features/visual_studio/creating_packages/build.bat
@@ -1,4 +1,4 @@
 @echo off
 
 conan create . demo/testing
-conan create . demo/testing -s compiler=msvc -s compiler.version=19.15 -s compiler.runtime=static -s compiler.cppstd=17
+conan create . demo/testing -s compiler=msvc -s compiler.version=19.16 -s compiler.runtime=static -s compiler.cppstd=17
