4e7124fb22ded85bad3f3f2058b56133e4c04548
César García Tapia <cesar.garcia.tapia@gmail.com>
2021-11-30 10:56:10 +0100

    Trying visual studio examples with msvc 19.13

--- features/visual_studio/reusing_packages/build.bat
+++ features/visual_studio/reusing_packages/build.bat
@@ -2,8 +2,8 @@
 
 pushd ..\creating_packages
 conan create . demo/testing
-conan create . demo/testing -s compiler=msvc -s compiler.version=19.14 -s compiler.runtime=static -s compiler.cppstd=17
+conan create . demo/testing -s compiler=msvc -s compiler.version=19.13 -s compiler.runtime=static -s compiler.cppstd=17
 popd
 
 conan create . demo/testing
-conan create . demo/testing -s compiler=msvc -s compiler.version=19.14 -s compiler.runtime=static -s compiler.cppstd=17
+conan create . demo/testing -s compiler=msvc -s compiler.version=19.13 -s compiler.runtime=static -s compiler.cppstd=17
