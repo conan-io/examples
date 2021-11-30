c204c5f853b49a73e145b802639380ff976d681c
César García Tapia <cesar.garcia.tapia@gmail.com>
2021-11-30 10:33:31 +0100

    Fix visual studio examples

--- features/visual_studio/creating_packages/build.bat
+++ features/visual_studio/creating_packages/build.bat
@@ -1,3 +1,4 @@
 @echo off
 
 conan create . demo/testing
+conan create . demo/testing -s compiler=msvc -s compiler.version=19.15 -s compiler.runtime=static -s compiler.cppstd=17
--- features/visual_studio/reusing_packages/build.bat
+++ features/visual_studio/reusing_packages/build.bat
@@ -2,6 +2,8 @@
 
 pushd ..\creating_packages
 conan create . demo/testing
+conan create . demo/testing -s compiler=msvc -s compiler.version=19.15 -s compiler.runtime=static -s compiler.cppstd=17
 popd
 
 conan create . demo/testing
+conan create . demo/testing -s compiler=msvc -s compiler.version=19.15 -s compiler.runtime=static -s compiler.cppstd=17
