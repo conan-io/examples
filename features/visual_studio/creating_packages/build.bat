ff5c975628e8d4eb9ac09a02c7d09fbc176a2cc1
César García Tapia <cesar.garcia.tapia@gmail.com>
2021-11-30 10:18:39 +0100

    Simplify the CI scripts

--- features/visual_studio/creating_packages/build.bat
+++ features/visual_studio/creating_packages/build.bat
@@ -1,4 +1,3 @@
 @echo off
 
 conan create . demo/testing
-conan create . demo/testing -s compiler=msvc -s compiler.version=19.29 -s compiler.runtime=static -s compiler.cppstd=17
--- features/visual_studio/reusing_packages/build.bat
+++ features/visual_studio/reusing_packages/build.bat
@@ -2,8 +2,6 @@
 
 pushd ..\creating_packages
 conan create . demo/testing
-conan create . demo/testing -s compiler=msvc -s compiler.version=19.29 -s compiler.runtime=static -s compiler.cppstd=17
 popd
 
 conan create . demo/testing
-conan create . demo/testing -s compiler=msvc -s compiler.version=19.29 -s compiler.runtime=static -s compiler.cppstd=17
