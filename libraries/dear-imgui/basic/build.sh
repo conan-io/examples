a8804a04e43fb0aa5372b2316380bac59f8c9712
Carlos Zoido <mrgalleta@gmail.com>
2022-07-28 09:54:30 +0200

    Update libraries/dear-imgui/basic/build.sh

--- libraries/dear-imgui/basic/build.sh
+++ libraries/dear-imgui/basic/build.sh
@@ -7,6 +7,7 @@ rm -rf build
 mkdir build
 pushd build
 
+export CONAN_SYSREQUIRES_MODE=enabled
 conan install .. --build=missing -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
 cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build .
