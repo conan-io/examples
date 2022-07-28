14c1eab6a98cf508309353cc6d266c4a223ae661
Carlos Zoido <mrgalleta@gmail.com>
2022-07-28 11:19:30 +0200

    Try to fix system package manager fail in ci (#110)
    * try to fix ci
    
    * add conf
    
    * Update libraries/dear-imgui/basic/build.sh

--- libraries/dear-imgui/basic/build.sh
+++ libraries/dear-imgui/basic/build.sh
@@ -8,6 +8,6 @@ mkdir build
 pushd build
 
 export CONAN_SYSREQUIRES_MODE=enabled
-conan install .. --build=missing
+conan install .. --build=missing -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
 cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build .
